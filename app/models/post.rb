b25a4e24d57cbf56259b0c3aa83f64b72c8ecb92
RoyOkite <royokite@gmail.com>
2022-11-26 03:11:47 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait_title
+
+    def clickbait_title
+        unless title.to_s.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+            errors.add(:title, "Invalid title, not clickbait-y enough")
+        end
+    end
 end
